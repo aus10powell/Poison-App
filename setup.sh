dc1022c410b1e3c1833840ceb90fe768ae3904b6
Austin Powell <aupowell@stanford.edu>
2020-11-03 09:46:08 -0800

    testing pushing to heroku

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"powella100@gmail.com\"\n\
+email = \"powella100@gmail.com git push heroku master\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
